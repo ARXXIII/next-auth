9014254f563b96638a90da58b8964fbd5d2cc468
Андрей Романов <human1488pwnz@yandex.ru>
2024-07-02 17:59:54 +0300

    Register functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,38 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
 
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	const validateFields = RegisterSchema.safeParse(values);
 
 	if (!validateFields.success) {
-		return { error: 'Invalid field!' };
+		return { error: 'Invalid fields!' };
 	}
 
-	return { success: 'Email sent!' };
+	const { name, email, password } = validateFields.data;
+
+	const hashedPassword = await bcrypt.hash(password, 12);
+
+	const existingUser = await getUserByEmail(email);
+
+	if (existingUser) {
+		return { error: 'Email already taken!' };
+	}
+
+	await db.user.create({
+		data: {
+			name,
+			email,
+			password: hashedPassword,
+		},
+	});
+
+	//TODO: Send verification token email
+
+	return { success: 'User created!' };
 };
