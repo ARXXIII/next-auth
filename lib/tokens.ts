9b7ecf217ab4b7ff18c6d12f3918b67f450622ff
Андрей Романов <human1488pwnz@yandex.ru>
2024-07-07 17:10:48 +0300

    2FA token generator

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,5 +1,9 @@
+import crypto from 'crypto';
+
 import { db } from '@/lib/db';
 import { v4 as uuidv4 } from 'uuid';
+
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
 import { getVerificationTokenEmail } from '@/data/verification-token';
 import { getPasswordResetTokenByEmail } from '@/data/password-reset-token';
 
@@ -51,4 +55,29 @@ export const generateResetToken = async (email: string) => {
 	});
 
 	return resetToken;
+};
+
+export const generateTwoFactorToken = async (email: string) => {
+	const token = crypto.randomInt(100000, 1000000).toString();
+	const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+	const existingToken = await getTwoFactorTokenByEmail(email);
+
+	if (existingToken) {
+		await db.twoFactorToken.delete({
+			where: {
+				id: existingToken.id,
+			},
+		});
+	}
+
+	const twoFactorToken = await db.twoFactorToken.create({
+		data: {
+			email,
+			token,
+			expires,
+		},
+	});
+
+	return twoFactorToken;
 };
