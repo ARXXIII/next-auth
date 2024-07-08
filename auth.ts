21a45daf13da7fe9512552b959868f0932309778
Андрей Романов <human1488pwnz@yandex.ru>
2024-07-08 21:29:04 +0300

    Added real-time data update

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import authConfig from '@/auth.config';
 import { db } from '@/lib/db';
 import { UserRole } from '@prisma/client';
 import { getUserById } from '@/data/user';
+import { getAccountByUserId } from './data/account';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { getTwoFactorConfirmationByUserId } from '@/data/two-factor-confirmation';
 
@@ -34,7 +35,7 @@ export const {
 			// Allow OAuth without email verification
 			if (account?.provider !== 'credentials') return true;
 
-			const existingUser = await getUserById(user.id);
+			const existingUser = await getUserById(user.id!);
 
 			// Prevent sign in without email verification
 			if (!existingUser?.emailVerified) return false;
@@ -65,6 +66,16 @@ export const {
 				session.user.role = token.role as UserRole;
 			}
 
+			if (token.isTwoFactorEnabled && session.user) {
+				session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+			}
+
+			if (session.user) {
+				session.user.name = token.name;
+				session.user.email = token.email!;
+				session.user.isOAuth = token.isOAuth as boolean;
+			}
+
 			return session;
 		},
 		async jwt({ token }) {
@@ -74,7 +85,13 @@ export const {
 
 			if (!existingUser) return token;
 
+			const existingAccount = await getAccountByUserId(existingUser.id);
+
+			token.isOAuth = !!existingAccount;
+			token.name = existingUser.name;
+			token.email = existingUser.email;
 			token.role = existingUser.role;
+			token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
 			return token;
 		},
